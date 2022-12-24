8946c88478d321c1384e5e8a3a6ff9fd385100f5
Ishan Tharaka <ishantharaka887@gmail.com>
2022-12-24 11:12:44 +0530

    Add Database,User Classes and Signup form

--- src/com/seekercloud/pos/controller/LoginFormController.java
+++ src/com/seekercloud/pos/controller/LoginFormController.java
@@ -1,4 +1,23 @@
 package com.seekercloud.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane loginFormContext;
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("SignUpForm");
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage window= (Stage) loginFormContext.getScene().getWindow();
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+
+    }
 }
---
+++ src/com/seekercloud/pos/db/Database.java
@@ -0,0 +1,9 @@
+package com.seekercloud.pos.db;
+
+import com.seekercloud.pos.model.User;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<User> userTable = new ArrayList<User>();
+}
