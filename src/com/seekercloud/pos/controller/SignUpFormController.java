ab34bbdb9bdb11a36913626ca07286beed3da984
Ishan Tharaka <ishantharaka887@gmail.com>
2022-12-24 11:35:52 +0530

    Signup page entered details added to the Database

--- src/com/seekercloud/pos/controller/SignUpFormController.java
+++ src/com/seekercloud/pos/controller/SignUpFormController.java
@@ -1,4 +1,47 @@
 package com.seekercloud.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.seekercloud.pos.db.Database;
+import com.seekercloud.pos.model.User;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+
 public class SignUpFormController {
+    public JFXTextField txtFullName;
+    public JFXPasswordField txtPassword;
+    public JFXTextField txtContact;
+    public JFXTextField txtEmail;
+    public JFXTextField txtRePassowrd;
+
+    public void signUpOnAction(ActionEvent actionEvent) {
+        // Check the password is matched?
+        String realPwd = txtPassword.getText().trim();  // __NDF_ After trim() ==> NDF
+        String matchPwd = txtRePassowrd.getText().trim();
+
+        if(!realPwd.equals(matchPwd)){
+            new Alert(Alert.AlertType.WARNING,"Both passwords should be matched !").show();
+            return;  // we will stop the JVM
+        }
+
+        User user = new User(
+            txtEmail.getText().trim(),txtFullName.getText().trim(),txtContact.getText().trim(),realPwd
+        );
+
+        if(saveUser(user)){
+            new Alert(Alert.AlertType.CONFIRMATION,"User Registered.").show();
+            clearFields();
+        }else {
+            new Alert(Alert.AlertType.WARNING,"Try Again !").show();
+        }
+    }
+
+    private void clearFields(){
+        txtEmail.clear();txtContact.clear();txtPassword.clear();
+        txtFullName.clear();txtRePassowrd.clear();
+    }
+
+    private boolean saveUser(User u){
+        return Database.userTable.add(u);   // inbuilt class ==> java.util
+    }
 }
